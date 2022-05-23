cd31f5fbe6d3b407ce8fae6f6e4122a9b764c7bc
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-05-23 17:20:16 +0600

    connected to mongodb atleast

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://welbim_manufacturing:<password>@cluster0.dhojc.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhojc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from Welbim!')
