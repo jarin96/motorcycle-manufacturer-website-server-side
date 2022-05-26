9b86c8598b66852079be2276280346a809c0065a
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-05-26 15:58:21 +0600

    done

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -43,6 +45,19 @@ async function run() {
             const cursor = partsCollection.find(query);
             const parts = await cursor.toArray();
             res.send(parts);
+        });
+
+        // Payment
+        app.post('/create-payment-intent', async (req, res) => {
+            const order = req.body;
+            const price = order.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
         });
 
 
@@ -176,6 +191,13 @@ async function run() {
             const result = await orders.find(userEmail).toArray();
             res.send(result);
 
+        })
+
+        app.get('/myOrder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await orders.findOne(query);
+            res.send(order);
         })
 
         // cancel order
